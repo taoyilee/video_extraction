233239d7d74a9d5693f694b178d5734533b88aaf
Michael Lee <tylee@ieee.org>
2018-03-07 11:19:41 +0800

    Add template matching scripts

--- app/video.py
+++ app/video.py
@@ -2,6 +2,7 @@ import configparser as cp
 import os
 
 import cv2
+import numpy as np
 
 
 class Video:
@@ -17,12 +18,12 @@ class Video:
     config: cp.ConfigParser = cp.ConfigParser()
     cap = None
     downsize_factor = 1
+    script_directory = os.path.dirname(os.path.realpath(__file__))
 
     def __init__(self, filename, config_file=None):
         self.filename = filename
         if config_file is None:
-            script_directory = os.path.dirname(os.path.realpath(__file__))
-            config_file = os.path.join(script_directory, "default_config.ini")
+            config_file = os.path.join(self.script_directory, "default_config.ini")
         print(f"Using config file {config_file}")
         if not os.path.isfile(config_file):
             raise FileNotFoundError(f"{config_file} does not exist")
@@ -61,6 +62,31 @@ class Video:
     def close(self):
         self.cap.release()
 
+    def match_template(self, img, template):
+        # methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
+        #           'cv2.TM_CCORR_NORMED', 'cv2.TM_SQDIFF', 'cv2.TM_SQDIFF_NORMED']
+        if not os.path.isfile(template):
+            raise FileNotFoundError(f"{config_file} does not exist")
+        temp_img = cv2.imread(template)
+        w, h = temp_img.shape[0:2]
+        # print(f"Template W/H = {w}/{h}")
+        method = eval('cv2.TM_CCOEFF')
+        res = cv2.matchTemplate(img, temp_img, method)
+        res -= np.min(res)
+        res /= np.max(res)
+        res *= 255
+        min_val, max_val, min_loc, max_loc = cv2.minMaxLoc(res)
+
+        if method in [cv2.TM_SQDIFF, cv2.TM_SQDIFF_NORMED]:
+            top_left = min_loc
+        else:
+            top_left = max_loc
+        bottom_right = (top_left[0] + w, top_left[1] + h)
+        cv2.rectangle(img, top_left, bottom_right, 255, 2)
+        res_out = np.zeros_like(img)
+        res_out[0:res.shape[0], 0:res.shape[1], :] = cv2.cvtColor(res, cv2.COLOR_GRAY2RGB)
+        return np.concatenate([res_out, img], axis=1)
+
     def extract_frames(self, show=False):
         self.rewind()
         output_dir = "captures"
@@ -79,9 +105,12 @@ class Video:
             font = cv2.FONT_HERSHEY_SIMPLEX
             frame_status = "{:.2f}s #{}".format(self.get_current_frame_time() / 1000, self.get_current_frame())
             gray = cv2.putText(gray, frame_status, (0, 13), font, 0.5, (0, 0, 255), 1, cv2.LINE_AA)
-            cv2.imshow('frame', gray)
+            cv2.imshow('frame', gray) if show else None
             scene_file_name = os.path.join(output_dir, "scene_{:03d}.png".format(frame_no))
+            detect_file_name = os.path.join(output_dir, "detect_{:03d}.png".format(frame_no))
             cv2.imwrite(scene_file_name, gray)
+            cv2.imwrite(detect_file_name,
+                        self.match_template(gray, os.path.join(self.script_directory, "sprites/00_huen.png")))
             frame_no += 1
             if cv2.waitKey(1) & 0xFF == ord('q'):
                 break
